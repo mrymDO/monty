6c500eb69c2c244d65c2de1b453387bddc54f532
s-maarouf <maarouf.salahedd@gmail.com>
2023-05-25 15:55:36 -0700

    Handling pchar

--- instructionFunc2.c
+++ instructionFunc2.c
@@ -42,3 +42,29 @@ void m_mod(stack_t **stack, unsigned int line_num)
 	after->n = res;
 	free(current);
 }
+
+/**
+ * m_pchar - prints the char at the top of the stack,
+ * followed by a new line
+ * @stack: pointer to the doubly linked list
+ * @line_num: number of lines
+ * Return: void
+*/
+
+void m_pchar(stack_t **stack, unsigned int line_num)
+{
+	stack_t *h;
+
+	h = *stack;
+	if (!h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	if (h->n > 127 || h->n < 0)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+	printf("%c\n", h->n);
+}
